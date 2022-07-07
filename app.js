63ff536e08972dc40b0284b05361ad8138f960fd
Janelle Alag <imjanellealag@gmail.com>
2022-07-07 23:02:59 +0800

    feat: setup app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+// Loads .env values to process.env
+dotenv.config();
+
+const setupExpress = async () => {
+    const app = express();
+
+    // Useful express middlewares
+    app.use(morgan('dev')); // HTTP request logger
+    app.use(express.json()); // Recognize request as JSON
+    app.use(express.urlencoded({ extended: false })); // Recognize request as strings
+    app.use(cookieParser()); // Parse Cookie header and populate req.cookies
+    app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+    // Extra middlewares
+    
+
+    // Listen to port
+    const server = app.listen(process.env.PORT || 3000, () => {
+        console.log(`Listening on port ${server.address().port}`)
+    });
+
+    return app;
+}
+
+module.exports = setupExpress().catch((error) => {
+    console.log(`Error starting server. Error: ${error}`);
+    process.exit(1);
+});
